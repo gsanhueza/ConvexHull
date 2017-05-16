f360070e3c199d4e15a97984df688a248a475a0d
gsanhueza <gabriel_8032@hotmail.com>
2017-05-16 18:53:43 -0400

    Se escribió un test para hacerlo funcionar o al menos detectar el error

--- ConvexHull.h
+++ ConvexHull.h
@@ -16,72 +16,20 @@ public:
     Poligono<T> quickHull(Punto<T> **cloud, const int numPoints);
 
 private:
-    Punto<T>* leftmostPoint(Punto<T> **cloud);
+    Punto<T>* leftmostPoint(Punto<T> **cloud, const int numPoints);
 };
 
-// FIXME Está mal implementado, se repiten 2 puntos todo el rato...
-// FIXME Cuando falla:
-/*
-Punto (404, 161)
-Punto (768, 703)
-Punto (894, 325)
-Punto (503, 711)
-Punto (711, 410)
-Punto (690, 905)
-Punto (841, 88)
-Punto (461, 300)
-cloud[0] is     Punto (404, 161)
-pointOnHull was Punto (404, 161)
-and endpoint is Punto (503, 711)
-
- *** Fallo: .../tarea_2 ha fallado ***
-*/
-// FIXME Cuando loopea:
-/*
-+Punto (658, 43)
-+Punto (502, 320)
-+Punto (298, 876)
-+Punto (275, 574)
-+Punto (631, 948)
-+Punto (743, 159)
-+Punto (787, 204)
-+Punto (148, 670)
-+cloud[0] is     Punto (658, 43)
-+pointOnHull was Punto (148, 670)
-+and endpoint is Punto (298, 876)
-+
-+cloud[0] is     Punto (658, 43)
-+pointOnHull was Punto (298, 876)
-+and endpoint is Punto (275, 574)
-+
-+cloud[0] is     Punto (658, 43)
-+pointOnHull was Punto (275, 574)
-+and endpoint is Punto (298, 876)
-+
-+cloud[0] is     Punto (658, 43)
-+pointOnHull was Punto (298, 876)
-+and endpoint is Punto (275, 574)
-+
-+cloud[0] is     Punto (658, 43)
-+pointOnHull was Punto (275, 574)
-+and endpoint is Punto (298, 876)
-+
-+cloud[0] is     Punto (658, 43)
-+pointOnHull was Punto (298, 876)
-+and endpoint is Punto (275, 574)
- */
-
+// FIXME Está mal implementado, se repiten 2 puntos todo el rato o se cae solo, revisar main...
 template<class T>
 Poligono<T> ConvexHull<T>::giftWrapping(Punto<T> **cloud, const int numPoints)
 {
 
-    Punto<T> *pointOnHull = leftmostPoint(cloud);
+    Punto<T> *pointOnHull = leftmostPoint(cloud, numPoints);
     Punto<T> *endpoint = new Punto<T>();
     Punto<T> **P = new Punto<T>*[numPoints];
     int i = 0;
 
     // Inicializador de puntos nuevos, no se usarán todos
-    // FIXME Si no se usan todos, entonces hay que saber cuáles sí se usan, y pasarlos al polígono nuevo!
     for (int k = 0; k < numPoints; k++)
     {
         P[k] = nullptr;
@@ -100,38 +48,45 @@ Poligono<T> ConvexHull<T>::giftWrapping(Punto<T> **cloud, const int numPoints)
             }
         }
         ++i;
-        cout << "cloud[0] is     " << *cloud[0] << endl;
-        cout << "pointOnHull was " << *pointOnHull << endl;
-        cout << "and endpoint is " << *endpoint << endl << endl;
         *pointOnHull = *endpoint;
     } while (not (*endpoint == *cloud[0]));
 
-    return Poligono<T>(numPoints, P);
+    // Detectamos puntos en ConvexHull
+    int k = 0;
+    for (; k < numPoints; k++)
+    {
+        if (P[k] == nullptr)
+        {
+            break;
+        }
+    }
+
+    return Poligono<T>(k, P);
 }
 
 template<class T>
 Poligono<T> ConvexHull<T>::quickHull(Punto<T> **cloud, const int numPoints)
 {
-    int i = 0;
-    do
+    for (int i = 0; i < numPoints; i++)
     {
         cout << *cloud[i] << endl;
-    } while (cloud[++i] != nullptr);
+    }
 
     Poligono<T> hull(numPoints, cloud);
     return hull;
 }
 
 template<class T>
-Punto<T> * ConvexHull<T>::leftmostPoint(Punto<T> **cloud)
+Punto<T> * ConvexHull<T>::leftmostPoint(Punto<T> **cloud, const int numPoints)
 {
     Punto<T> *leftmost = cloud[0];
     int i = 0;
-    while (cloud[++i] != nullptr)
+
+    for (i = 1; i < numPoints; i++)
     {
         if (cloud[i]->getX() < leftmost->getX())
         {
-            leftmost = cloud[i];
+            *leftmost = *cloud[i];
         }
     }
 
