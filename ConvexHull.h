a821ceb12dfb6dbf612b0672cc0b11c0b8b85076
gsanhueza <gabriel_8032@hotmail.com>
2017-05-16 18:13:58 -0400

    Hay 2 formas de hacer caer esto, están escritas ahora

--- ConvexHull.h
+++ ConvexHull.h
@@ -20,6 +20,57 @@ private:
 };
 
 // FIXME Está mal implementado, se repiten 2 puntos todo el rato...
+// FIXME Cuando falla:
+/*
+Punto (404, 161)
+Punto (768, 703)
+Punto (894, 325)
+Punto (503, 711)
+Punto (711, 410)
+Punto (690, 905)
+Punto (841, 88)
+Punto (461, 300)
+cloud[0] is     Punto (404, 161)
+pointOnHull was Punto (404, 161)
+and endpoint is Punto (503, 711)
+
+ *** Fallo: .../tarea_2 ha fallado ***
+*/
+// FIXME Cuando loopea:
+/*
++Punto (658, 43)
++Punto (502, 320)
++Punto (298, 876)
++Punto (275, 574)
++Punto (631, 948)
++Punto (743, 159)
++Punto (787, 204)
++Punto (148, 670)
++cloud[0] is     Punto (658, 43)
++pointOnHull was Punto (148, 670)
++and endpoint is Punto (298, 876)
++
++cloud[0] is     Punto (658, 43)
++pointOnHull was Punto (298, 876)
++and endpoint is Punto (275, 574)
++
++cloud[0] is     Punto (658, 43)
++pointOnHull was Punto (275, 574)
++and endpoint is Punto (298, 876)
++
++cloud[0] is     Punto (658, 43)
++pointOnHull was Punto (298, 876)
++and endpoint is Punto (275, 574)
++
++cloud[0] is     Punto (658, 43)
++pointOnHull was Punto (275, 574)
++and endpoint is Punto (298, 876)
++
++cloud[0] is     Punto (658, 43)
++pointOnHull was Punto (298, 876)
++and endpoint is Punto (275, 574)
+ */
+
 template<class T>
 Poligono<T> ConvexHull<T>::giftWrapping(Punto<T> **cloud, const int numPoints)
 {
@@ -33,19 +84,19 @@ Poligono<T> ConvexHull<T>::giftWrapping(Punto<T> **cloud, const int numPoints)
     // FIXME Si no se usan todos, entonces hay que saber cuáles sí se usan, y pasarlos al polígono nuevo!
     for (int k = 0; k < numPoints; k++)
     {
-        P[k] = new Punto<T>();
+        P[k] = nullptr;
     }
 
     do
     {
-        *P[i] = *pointOnHull;
+        P[i] = new Punto<T>(*pointOnHull);
         *endpoint = *cloud[0];
         for (int j = 1; j < numPoints; ++j)
         {
             Segmento<T> seg(*P[i], *endpoint);
             if (*endpoint == *pointOnHull or seg.isThisPointAtLeft(*cloud[j]))
             {
-                *endpoint = *cloud[j];                      // FIXME Funciona si le saco los punteros, pero no debería caerse aquí...
+                *endpoint = *cloud[j];
             }
         }
         ++i;
