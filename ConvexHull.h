927efc28391d3623aedcf0e59e30ae547f5b7983
gsanhueza <gabriel_8032@hotmail.com>
2017-05-19 08:51:38 -0400

    Código arreglado, ahora es legible

--- ConvexHull.h
+++ ConvexHull.h
@@ -23,6 +23,7 @@ private:
     void lowestPoint(vector<Punto<T>> &cloud, int &lowestPos);
     void swapPoints(vector<Punto<T>> &cloud, const int a, const int b);
     void polarSort(vector<Punto<T>> &cloud, Punto<T> &p0);
+    inline int orient(Punto<T> &p0, Punto<T> &p1, Punto<T> &p2);
 };
 
 template<class T>
@@ -40,10 +41,8 @@ Poligono<T> ConvexHull<T>::giftWrapping(vector<Punto<T>> &cloud)
         int q = (p + 1) % cloud.size();
         for (int i = 0; i < cloud.size(); i++)
         {
-            int val = (cloud.at(p).getY() - cloud.at(i).getY()) * (cloud.at(q).getX() - cloud.at(p).getX()) -
-                      (cloud.at(p).getX() - cloud.at(i).getX()) * (cloud.at(q).getY() - cloud.at(p).getY());
-
-            if (val > 0)                                    // CCW
+            // CCW
+            if (orient(cloud.at(i), cloud.at(q), cloud.at(p)) > 0)
             {
                 q = i;
             }
@@ -66,7 +65,8 @@ Poligono<T> ConvexHull<T>::grahamScan(vector<Punto<T>> &cloud)
 
     for (int i = 1; i < cloud.size(); i++)
     {
-        while (i < cloud.size() - 1 and Poligono<T>(3, cloud.at(0), cloud.at(i), cloud.at(i + 1)).area() == 0)
+
+        while (i < cloud.size() - 1 and orient(cloud.at(i), cloud.at(i + 1), cloud.at(0)) == 0)
         {
             i++;
         }
@@ -82,7 +82,7 @@ Poligono<T> ConvexHull<T>::grahamScan(vector<Punto<T>> &cloud)
 
     for (int i = 3; i < M; i++)
     {
-        while (not Poligono<T>(3, hull.at(hull.size() - 2), hull.back(), cloud.at(i)).isCCW())
+        while (orient(hull.back(), cloud.at(i), hull.at(hull.size() - 2)) < 0)
         {
             hull.pop_back();
         }
@@ -159,5 +159,13 @@ void ConvexHull<T>::polarSort(vector<Punto<T> >& cloud, Punto<T> &p0)
         return val > 0;
     });
 }
+
+template<class T>
+inline int ConvexHull<T>::orient(Punto<T>& p0, Punto<T>& p1, Punto<T>& p2)
+{
+    return  (p2.getY() - p0.getY()) * (p1.getX() - p2.getX()) -
+            (p2.getX() - p0.getX()) * (p1.getY() - p2.getY());
+}
+
 
 #endif // CONVEX_HULL_H
